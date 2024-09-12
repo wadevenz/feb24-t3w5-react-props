6beffa78b942564dabe87af885344f670d30aeea
Wade <158378370+wadevenz@users.noreply.github.com>
2024-09-12 21:02:54 +1000

    feat: installed react-bootstrap and added cards

--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -1,10 +1,18 @@
+import Card from 'react-bootstrap/Card';
+
 
 export default function FoodCard(props) {
     const {foodName} = props
     return(
-        <div>
-            <h3>{foodName}</h3>
-            <p>{props.foodName}</p>
-        </div>
+        <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>{foodName}</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
     )
 }
